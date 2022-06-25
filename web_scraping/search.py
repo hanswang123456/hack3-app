4215125b7d7c2a7bb001e12cf0590e639b085274
japrozs <sainijaproz@gmail.com>
2022-06-26 00:20:34 +0530

    commit

--- web_scraping/search.py
+++ web_scraping/search.py
@@ -1,19 +1,4 @@
 from googlesearch import search
 
-# to search
-query = "Geeksforgeeks"
-
-for j in search(query):
-    print(j)
-
-# https://in.linkedin.com/company/geeksforgeeks
-# https://www.instagram.com/geeks_for_geeks/?hl=en
-# https://play.google.com/store/apps/details?id=free.programming.programming&hl=en_IN&gl=US
-# https://www.crunchbase.com/organization/geeks-for-geeks
-# https://in.linkedin.com/company/geeksforgeeks
-# https://www.instagram.com/geeks_for_geeks/?hl=en
-# https://play.google.com/store/apps/details?id=free.programming.programming&hl=en_IN&gl=US
-# https://www.crunchbase.com/organization/geeks-for-geeks
-# https://www.glassdoor.co.in/Overview/Working-at-GeeksForGeeks-EI_IE1457375.11,24.htm
-# https://www.crunchbase.com/organization/geeks-for-geeks
-# https://www.quora.com/What-is-GeeksforGeeks-How-can-I-use-it
+def search():
+        return search(query)
