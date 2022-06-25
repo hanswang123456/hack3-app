9e4ea2582f9f500072356d6591886d039a11cf38
vsahni3 <varunsahni3504@gmail.com>
2022-06-25 13:27:10 -0400

    initialize

--- get_urls.py
+++ get_urls.py
@@ -1,7 +1,56 @@
 import requests
-import bs4
-a = 'https://www.google.com/search?q=anime+op+mc&rlz=1C1UEAD_enCA963CA963&oq=anime+op+mc&aqs=chrome.0.69i59l3j69i57j69i60l4.1485j0j7&sourceid=chrome&ie=UTF-8&safe=active&ssui=on'
-b = 'https://www.google.com/search?q=anime+enemies+to+lovers&rlz=1C1UEAD_enCA963CA963&ei=BCa3YqjICI2GtQaFir34Bg&ved=0ahUKEwio2tL-8cj4AhUNQ80KHQVFD28Q4dUDCA8&uact=5&oq=anime+enemies+to+lovers&gs_lcp=Cgdnd3Mtd2l6EAMyBAgAEEcyBAgAEEcyBAgAEEcyBAgAEEcyBAgAEEcyBAgAEEcyBAgAEEcyBAgAEEdKBAhBGABKBAhGGABQlhtYiS1g1C1oAHADeACAAQCIAQCSAQCYAQCgAQHIAQjAAQE&sclient=gws-wiz&safe=active&ssui=on'
-for i in range(len(a)):
-    if a[i] != b[i]:
-        print(a[i], b[i])
+import urllib
+import pandas as pd
+from requests_html import HTML
+from requests_html import HTMLSession
+from googlesearch import search   
+
+def method1():
+    query = "anime enemies to lovers"
+
+    links = []
+    for j in search(query, tld="co.in", num=10, stop=10, pause=2): 
+        links.append(j)
+    print(links)
+
+def method2():
+
+    def get_source(url):
+        """Return the source code for the provided URL. 
+
+        Args: 
+            url (string): URL of the page to scrape.
+
+        Returns:
+            response (object): HTTP response object from requests_html. 
+        """
+
+        try:
+            session = HTMLSession()
+            response = session.get(url)
+            return response
+
+        except requests.exceptions.RequestException as e:
+            print(e)
+
+    def scrape_google(query):
+
+        query = urllib.parse.quote_plus(query)
+        response = get_source("https://www.google.co.uk/search?q=" + query)
+
+        links = list(response.html.absolute_links)
+        google_domains = ('https://www.google.', 
+                        'https://google.', 
+                        'https://webcache.googleusercontent.', 
+                        'http://webcache.googleusercontent.', 
+                        'https://policies.google.',
+                        'https://support.google.',
+                        'https://maps.google.')
+
+        for url in links[:]:
+            if url.startswith(google_domains):
+                links.remove(url)
+
+        return links
+
+    print(scrape_google("anime op mc"))
